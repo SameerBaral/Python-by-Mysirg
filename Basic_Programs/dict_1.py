788907f22489762c0434deb4d54405d168485b36
Sameer Baral <baralsameer70@gmail.com>
2026-08-25 20:04:14 +0530

    updated dict-1

--- Basic_Programs/dict_1.py
+++ Basic_Programs/dict_1.py
@@ -23,13 +23,14 @@
 # d1 = {}     #empty object
 # print(d1)
 
+# dict function se pass karega to key string hi ho skta hai
 # d1 = dict(a=10,b=20,c=30)
 # print(d1)
 
 
 # Accessing dict elements:-
 
-d1 = {101:'Rahul',103:'Payal',104:'Arjun',105:'Prachi'}
+# d1 = {101:'Rahul',103:'Payal',104:'Arjun',105:'Prachi'}
 # print(d1[101])   # Rahul
 
 # har key ko access karne ke liye for loop ka use kr skte hai
@@ -44,6 +45,41 @@ d1 = {101:'Rahul',103:'Payal',104:'Arjun',105:'Prachi'}
 
 # dict class functions:-ye sb iterable hote hai
 
-print(d1.items())
-print(d1.keys())
-print(d1.values())
+# print(d1.items())
+# print(d1.keys())
+# print(d1.values())
+
+
+# for v in d1.values():
+#     print(v)  
+
+# key-value tuple
+# for e in d1.items():
+#     print(e)  
+
+# unpacking tuple elements
+# for k,v in d1.items():
+#     print(k,v) 
+
+
+
+# ------------------------------------
+# dict class functions:-
+
+# pop, popitem, clear, del, update, copy, get, setdefault, fromkeys
+# d1.pop(103)  # key 103 ko delete kr dega
+# print(d1)  
+
+
+# d1.popitem()  # last element ko delete kr dega
+# print(d1)
+
+# key 101 ka value return karega
+# print(d1.get(101))  # Rahul
+
+
+
+# dict comprehension:-
+
+# d3 = {x:x**2 for x in range(1,6)}
+# print(d3)
