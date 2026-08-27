ed1853c66aae9e86807cbebb4d890a64dc0e1eab
Sameer Baral <baralsameer70@gmail.com>
2026-08-27 18:24:57 +0530

    Assignment20 completed

---
+++ Assignments/Assig-20/A20.py
@@ -0,0 +1,38 @@
+
+# Que-1
+
+# for i in range(1, 11):
+#     print(5*i, end=" ")
+
+
+
+# Que-2
+
+# n = int(input("Enter a number: "))
+# for i in range(1, 11):
+#     print(n*i, end=" ")
+
+
+
+# Que-3
+
+# n = int(input("Enter a number: "))
+# m = int(input("Enter a number how many multiple you want: "))
+# for i in range(1, m+1):
+#     print(n*i, end=" ")
+
+
+
+# Que-4
+
+# n = int(input("Enter a number: "))
+# for i in range(10, 0, -1):
+#     print(n*i, end=" ")
+
+
+
+# Que-5
+
+# n = int(input("Enter a number: "))
+# for i in range(1, 11):
+#     print(n*i, end=" ")
