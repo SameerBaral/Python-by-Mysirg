c5cd41395aec4158861267a2d520721eae2aac16
Sameer Baral <baralsameer70@gmail.com>
2026-08-27 18:53:39 +0530

    Assignment-21 complete

---
+++ Assignments/Assig-21/A21.py
@@ -0,0 +1,44 @@
+
+
+# Que-1
+
+# n = int(input("Enter a number: "))
+# for i in range(1, n+1):
+#     print(2*i, end=" ")
+
+
+
+# Que-2
+
+# n = int(input("Enter a number: "))
+# for i in range(1, n+1):
+#     print(2*i-1, end=" ")
+
+
+
+# Que-3
+
+# n = int(input("Enter a number: "))
+# for i in range(1, n+1):
+#     print(i**2, end=" ")
+
+
+
+# Que-4
+
+# n = int(input("Enter a number: "))
+# for i in range(1, n+1):
+#     print(i**3, end=" ")
+
+
+
+# Que-5
+
+# start = int(input("Enter the starting number: "))
+# end = int(input("Enter the ending number: "))
+# for x in range(start, end):
+#     for i in range(2,x):
+#         if x % i == 0:
+#             break
+#     else:
+#         print(x,end=" ")
