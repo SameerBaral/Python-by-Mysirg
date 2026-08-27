2215a92168a67674dbabc0bfa842dc8f015cba6b
Sameer Baral <baralsameer70@gmail.com>
2026-08-27 17:36:31 +0530

    Assignment-19 completed

---
+++ Assignments/Assig-19/A19.py
@@ -0,0 +1,57 @@
+
+# Que-1
+
+# s1 = input("Enter a string: ")
+# for i in s1:
+#     print(i, ord(i))
+
+
+
+# Que-2
+
+# s1 = input("Enter a string: ")
+# for i in s1:
+#     if i in "aeiouAEIOU":
+#         print(i)
+
+
+
+# Que-3
+
+# s1 = input("Enter a string: ")
+# count = 0
+# for i in s1:
+#     if i == " ":
+#         count+=1
+# print("Number of spaces in the string: ", count)
+
+
+
+# Que-4
+# ex=> 214218 ==> 2148 print only unique digits
+
+# num = input("Enter a number: ")
+# unique_digits = ""
+# for i in num:
+#     if i not in unique_digits:
+#         unique_digits += i
+# print("Unique digits in the number: ", unique_digits)
+
+# or
+
+# num = input("Enter a number: ")
+# i=0
+# for e in num:
+#     if num.index(e) == i:
+#         print(e, end="")
+#     i+=1
+
+
+
+# Que-5
+
+# num = input("Enter a number: ")
+# count = 0
+# for i in num:
+#     count+=1
+# print("Total digits in the number: ", count)
