8cea846c016c8e506e83976b088ab189729cffe6
Sameer Baral <baralsameer70@gmail.com>
2026-08-27 19:09:46 +0530

    Assignment-22 complete

---
+++ Assignments/Assig-22/A22.py
@@ -0,0 +1,49 @@
+
+
+# Que-1
+
+# n = int(input("Enter a number: "))
+# s = 0
+# for i in range(1, n + 1):
+#     s += i
+# print("The sum of first", n, "natural numbers is:", s)
+
+
+
+# Que-2
+
+# n = int(input("Enter a number: "))
+# s = 0
+# for i in range(1, n + 1):
+#     s += i ** 2
+# print("The sum of squares of first", n, "natural numbers is:", s)
+
+
+
+# Que-3
+
+# n = int(input("Enter a number: "))
+# s = 0
+# for i in range(1, n + 1):
+#     s += i ** 3
+# print("The sum of cubes of first", n, "natural numbers is:", s)
+
+
+
+# Que-4
+
+# n = int(input("Enter a number: "))
+# s = 0
+# for i in range(1, n+1):
+#     s+= 2*i-1
+# print("The sum of first", n, "odd natural numbers is:", s)
+
+
+
+# Que-5
+
+# n = int(input("Enter a number: "))
+# s = 0
+# for i in range(1, n + 1):
+#     s += 2 * i
+# print("The sum of first", n, "even natural numbers is:", s)
