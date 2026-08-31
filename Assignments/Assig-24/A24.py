2b7792f9d52e9d2f291dac57f972372ba6fdfcbc
Sameer Baral <baralsameer70@gmail.com>
2026-08-31 18:05:57 +0530

    Assignment 24 completed

---
+++ Assignments/Assig-24/A24.py
@@ -0,0 +1,50 @@
+
+# Que-1
+
+# print("Enter number separated by comma:")
+# l1 = [int(e) for e in input().split(',')]
+# s = sum(l1)
+# print("Sum of numbers:", s)
+
+
+
+
+# Que-2
+
+# print("Enter number separated by comma:")
+# l1 = [int(e) for e in input().split(',')]
+# avg = sum(l1)/len(l1)
+# print("Average of numbers:", avg)
+
+
+
+
+# Que-3
+
+# print("Enter number separated by comma:")
+# l1 = [int(e) for e in input().split(',')]
+# l2 = [e**2 for e in l1]
+# print("Squares of numbers:", l2)
+
+
+
+# Que-4
+
+# print("Enter number separated by comma:")
+# l1 = [int(e) for e in input().split(',')]
+# l1.sort(reverse=True)
+# print("Numbers in descending order:", l1)
+
+
+
+# Que-5
+
+# print("Enter number separated by comma:")
+# l1 = [int(e) for e in input().split(',')]
+# i = 1
+# l2 = []
+# for e in l1:
+#     if i % 2 == 0:
+#         l2.append(e)
+#     i += 1
+# print("Numbers at even positions:", l2)
