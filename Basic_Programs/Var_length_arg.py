458d66d3376428f4887e0aeb2733e8d66fcf298d
Sameer Baral <baralsameer70@gmail.com>
2026-09-04 23:08:14 +0530

    var length argument

---
+++ Basic_Programs/Var_length_arg.py
@@ -0,0 +1,19 @@
+# Var length Argument:-
+
+# Write a function to calculate average of two number
+
+# def average(a,b,c):
+#     avg = (a+b+c)/3
+#     return avg
+
+# print(average(10,20,30))
+
+# -------------------------------
+
+# here t is a tuple
+# def average(*t):
+#     avg = sum(t)/len(t)
+#     return avg
+
+# print(average(10,20,30,40))
+# print(average(10,20))
