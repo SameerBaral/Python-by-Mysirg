42de2f8aa2e64497a41f6b9cdbd00cf75d6a1dca
Sameer Baral <baralsameer70@gmail.com>
2026-09-04 23:34:30 +0530

    recursion

---
+++ Basic_Programs/recursion.py
@@ -0,0 +1,13 @@
+# Recursion :-
+
+
+# sum of n natural numbers
+
+# def fun1(n):
+#     if n==1:  #base case
+#         return 1
+#     s = n + fun1(n-1)
+#     return s
+
+# result = fun1(3)
+# print(result)
