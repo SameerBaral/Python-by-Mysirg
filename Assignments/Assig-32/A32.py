4a1d9c98c9011696b0849ad8fb1548c0df5d2080
Sameer Baral <baralsameer70@gmail.com>
2026-09-05 19:54:03 +0530

    Assignment 32 complete

---
+++ Assignments/Assig-32/A32.py
@@ -0,0 +1,49 @@
+# Function-1 (Take Something, Return Something)
+
+
+# Que-01
+
+# def add(a,b):
+#     return a+b
+
+# print("The sum is:", add(10,20))
+
+
+
+
+# Que-02
+
+# def areaOfCircle(r):
+#     return 3.14*r*r
+
+# print("The area of circle is:", areaOfCircle(5))
+
+
+
+
+# Que-03
+
+# def average(a,b,c):
+#     return (a+b+c)/3
+
+# print("The average is:", average(10,20,30))
+
+
+
+# Que-04
+
+# def compoundInterest(p,r,t):
+#     A = p*(1+r/100)**t
+#     return A-p
+
+# print("The compound interest is:", compoundInterest(1000,5,2))
+
+
+
+
+# Que-05
+
+# def volumeOfCuboid(l,b,h):
+#     return l*b*h
+
+# print("The volume of cuboid is:", volumeOfCuboid(10,20,30))
