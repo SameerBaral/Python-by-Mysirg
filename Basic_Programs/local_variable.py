baae23f2ea5877da888af24b3c48e478394446e3
Sameer Baral <baralsameer70@gmail.com>
2026-09-06 13:47:24 +0530

    local variable

---
+++ Basic_Programs/local_variable.py
@@ -0,0 +1,46 @@
+# local and global variable
+
+# local variable:-
+
+# def f1():
+#     x = 10
+#     print(x)
+
+# f1()
+# # print(x)
+
+
+
+
+
+# Global variable :-
+
+# x = 20
+# def f1():
+#     print(x)
+
+# f1() 
+# x = 30 
+# print(x)
+
+
+# -----------------------------
+
+# x = 20  #global variable
+# def f1():
+#     x = 10 #local variable
+#     print(x)
+
+# f1()
+# print(x)
+
+# -----------------------------------
+
+# x = 20  #global variable
+# def f1():
+#     global x
+#     x = 10 #global variable
+#     print(x)
+
+# f1()
+# print(x)
